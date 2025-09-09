c0252e52fd77f782cb2e5351f3fbd65ca8d267be
JFT29 <juansao.fortunio@ui.ac.id>
2025-09-10 00:41:53 +0700

    feat(main): app + routing + view + template (app/name/class)

---
+++ main/views.py
@@ -0,0 +1,9 @@
+﻿from django.shortcuts import render
+
+def show_main(request):
+    context = {
+        "app_name": "KickoffKart",
+        "your_name": "Juansao Fortunio",
+        "your_class": "PBP Ganjil 2025/2026",
+    }
+    return render(request, "main.html", context)
